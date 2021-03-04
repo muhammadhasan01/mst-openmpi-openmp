8b5baf0e6d28eaf5d4789b35fe2c8b6bceb43d64
naufal-dean <13518123@std.stei.itb.ac.id>
2021-03-05 00:31:56 +0700

    chore: add mpi

--- src/MST_OpenMPI.c
+++ src/MST_OpenMPI.c
@@ -80,51 +80,142 @@ void merge_sort(edge edges[], int n, int (*comparison)(edge*, edge*)) {
 }
 
 int main(int argc, char** argv) {
+    int world_size, world_rank;
+
     MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+    // Define mpi type
+    MPI_Datatype MPI_EDGE;
+    // Type create struct
+    int block_count = 3;
+    const int block_lengths[3] = { 1, 1, 1 };
+    const MPI_Aint displacements[3] = { 0, sizeof(int), sizeof(int) * 2 };
+    MPI_Datatype block_types[3] = { MPI_INT, MPI_INT, MPI_INT };
+    MPI_Type_create_struct(block_count, block_lengths, displacements, block_types, &MPI_EDGE);
+    // Commit type
+    MPI_Type_commit(&MPI_EDGE);
+
+    // Input
     clock_t t = clock();
-    scanf("%d", &n);
-    edge* edges = (edge*) malloc(n * n * sizeof(edge));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int x;
-            scanf("%d", &x);
-            if (x == -1) continue;
-            if (i >= j) continue;
-            edges[num_edge].u = i;
-            edges[num_edge].v = j;
-            edges[num_edge].w = x;
-            num_edge++;
+    if (world_rank == 0) {
+        scanf("%d", &n);
+        edges = (edge*) malloc(n * n * sizeof(edge));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int x;
+                scanf("%d", &x);
+                if (x == -1) continue;
+                if (i >= j) continue;
+                edges[num_edge].u = i;
+                edges[num_edge].v = j;
+                edges[num_edge].w = x;
+                num_edge++;
+            }
         }
+        assert(num_edge >= n - 1);
     }
-    assert(num_edge >= n - 1);
-    merge_sort(edges, num_edge, comparison_weight);
+    MPI_Barrier(MPI_COMM_WORLD);
 
-    par = (int*) malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++) {
-        par[i] = i;
+    if (world_rank == 0) {
+        for (int i = 1; i < world_size; i++)
+            MPI_Send(&num_edge, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Recv(&num_edge, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
-    long long total_cost = 0;
+
+    // Process
+    {
+        // Define array for gather and scatter
+        edge gathered[num_edge];
+        int scattered_size = num_edge / world_size;
+        edge scattered[scattered_size + 1];
+
+        int sendcounts[world_size];
+        int displs[world_size];
+        int rem = num_edge % world_size;
+        int sum = 0;
+        for (int i = 0; i < world_size; i++) {
+            sendcounts[i] = num_edge / world_size + (i < rem);
+            displs[i] = sum;
+            sum += sendcounts[i];
+        }
+
+        // Scatter and gather
+        MPI_Scatterv(edges, sendcounts, displs, MPI_EDGE, scattered, sendcounts[world_rank], MPI_EDGE, 0, MPI_COMM_WORLD);
+        merge_sort(scattered, sendcounts[world_rank], comparison_weight);
+        MPI_Gatherv(scattered, sendcounts[world_rank], MPI_EDGE, gathered, sendcounts, displs, MPI_EDGE, 0, MPI_COMM_WORLD);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
     int num_chosen = 0;
-    chosen_edges = (edge*) malloc(num_edge * sizeof(edge));
-    for (int i = 0; i < num_edge; i++) {
-        int u = edges[i].u;
-        int v = edges[i].v;
-        int w = edges[i].w;
-        if (merge_set(u, v)) {
-            total_cost += w;
-            chosen_edges[num_chosen++] = edges[i];
-            if (num_chosen == n - 1) break;
+    long long total_cost = 0;
+    if (world_rank == 0) {
+        par = (int*) malloc(n * sizeof(int));
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+        }
+        chosen_edges = (edge*) malloc(num_edge * sizeof(edge));
+        for (int i = 0; i < num_edge; i++) {
+            int u = edges[i].u;
+            int v = edges[i].v;
+            int w = edges[i].w;
+            if (merge_set(u, v)) {
+                total_cost += w;
+                chosen_edges[num_chosen++] = edges[i];
+                if (num_chosen == n - 1) break;
+            }
         }
     }
-    printf("%lld\n", total_cost);
-    merge_sort(chosen_edges, num_chosen, comparison_node);
+    MPI_Barrier(MPI_COMM_WORLD);
 
-    for (int i = 0; i < num_chosen; i++) {
-        printf("%d-%d\n", chosen_edges[i].u, chosen_edges[i].v);
+    if (world_rank == 0) {
+        for (int i = 1; i < world_size; i++)
+            MPI_Send(&num_chosen, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Recv(&num_chosen, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
-    double time_taken = ((double) (clock() - t)) / CLOCKS_PER_SEC;
-    printf("Waktu Eksekusi: %f ms\n", time_taken);
 
+    {
+        // Define array for gather and scatter
+        edge gathered[num_chosen];
+        int scattered_size = num_chosen / world_size;
+        edge scattered[scattered_size + 1];
+
+        int sendcounts[world_size];
+        int displs[world_size];
+        int rem = num_chosen % world_size;
+        int sum = 0;
+        for (int i = 0; i < world_size; i++) {
+            sendcounts[i] = num_chosen / world_size + (i < rem);
+            displs[i] = sum;
+            sum += sendcounts[i];
+        }
+
+        // Scatter and gather
+        MPI_Scatterv(chosen_edges, sendcounts, displs, MPI_EDGE, scattered, sendcounts[world_rank], MPI_EDGE, 0, MPI_COMM_WORLD);
+        merge_sort(scattered, sendcounts[world_rank], comparison_node);
+        MPI_Gatherv(scattered, sendcounts[world_rank], MPI_EDGE, gathered, sendcounts, displs, MPI_EDGE, 0, MPI_COMM_WORLD);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    // Output
+    if (world_rank == 0) {
+        printf("%lld\n", total_cost);
+
+        for (int i = 0; i < num_chosen; i++) {
+            printf("%d-%d\n", chosen_edges[i].u, chosen_edges[i].v);
+        }
+        double time_taken = ((double) (clock() - t)) / CLOCKS_PER_SEC;
+        printf("Waktu Eksekusi: %f ms\n", time_taken);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // End
+    MPI_Type_free(&MPI_EDGE);
+    MPI_Finalize();
     return 0;
 }
